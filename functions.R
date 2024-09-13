ba044109a6fe3f268d8096bed52ef42d6f9d9791
Валерия Патуева <valeria@MBP-Valeria.fritz.box>
2024-09-13 16:23:38 +0200

    changes

--- functions.R
+++ functions.R
@@ -1,5 +1,13 @@
 # basic functions
 
+# 1. input a time-travel matrix produced by r5r package
+# 2. input a reference (root) point
+# 3. select a destination point
+# 4. find the shortest path
+# 5. calculate new coordinates of each point on this path
+
+#----new points----
+
 # new points
 new_points <- function(points, time_matrix, speed = 0.5) {
   # Initialize a data frame to store new points
@@ -69,3 +77,69 @@ ggplot() +
   geom_point(data = points, aes(x = x, y = y), color = "blue", size = 3, shape = 16) +
   geom_point(data = new_points_df, aes(x = x, y = y), color = "red", size = 4, shape = 17, alpha = 0.5) +
   theme_minimal()
+
+
+#----real data----
+options(timeout = 1200)
+install.packages("usethis")
+
+# packages
+library(tidyverse)
+library(sf)
+library(tidytransit)
+library(usethis)
+library(r5r)
+library(osmextract)
+
+# OSM
+# usethis::edit_r_environ()
+# Add a line containing: OSMEXT_DOWNLOAD_DIRECTORY=/path/to/save/files
+oe_download_directory()
+osm_han = oe_get("Hannover", quiet = FALSE)
+highway <- osm_han["highway"]
+table(highway$highway)
+bbox <- st_as_sfc(st_bbox(highway), crs = 4326)
+class(bbox)
+
+# data
+setwd("/Users/valeria/GTFS")
+area <- read_sf("region_hannover.geojson")
+gtfs_han <- tidytransit::read_gtfs("gtfs_han.zip")
+
+# stops
+stops <- stops_as_sf(gtfs_han$stops)
+stops <- st_join(stops, area, join = st_within) %>% filter(!is.na(NUTS)) %>%
+  #st_drop_geometry() %>%
+  select(stop_name) %>%
+  group_by(stop_name) %>%
+  slice(1) %>%
+  ungroup()
+
+# visual
+highway2 <- highway %>% filter(highway == "primary" | highway == "secondary")
+ggplot() +
+  geom_sf(data = st_geometry(stops)) +
+  geom_sf(data = highway2)
+  #geom_sf(data = bbox, aes(col = "grey", alpha = 0.5))
+
+# Java problems
+#library(rJavaEnv)
+#rJavaEnv::java_quick_install(version = 21)
+#use_java(21)
+#java_check_version_cmd()
+#java_check_version_rjava()
+#rJava::.jcall("java.lang.System", "S", "getProperty", "java.version")
+# sudo R CMD javareconf JAVA_HOME=/Users/valeria/Library/Caches/org.R-project.R/R/rJavaEnv/installed/macos/x64/21
+
+# network
+options(java.parameters = '-Xmx8G')
+#r5r::download_r5(force_update = TRUE)
+r5r_core <- setup_r5("/Users/valeria/GTFS")
+
+
+
+plot(st_geometry(stops))
+
+
+
+
